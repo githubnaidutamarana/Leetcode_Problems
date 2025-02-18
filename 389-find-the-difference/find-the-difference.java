80a1522ef236897d3ca8b8eb086b835f40bfaafa
NAIDU TAMARANA <149986263+githubnaidutamarana@users.noreply.github.com>
2025-02-18 17:52:56 +0530

    Time: 3 ms (40.40%) | Memory: 41.8 MB (77.70%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,16 +1,8 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int ssum = 0;
-        int tsum =0;
-        for(int i=0;i<s.length();i++)
-        {
-            ssum = ssum + (int)s.charAt(i);
-        }
-        for(int i=0;i<t.length();i++)
-        {
-            tsum = tsum + (int)t.charAt(i);
-        }
-        int value = tsum - ssum;
-        return (char)value;    
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) result ^= s.charAt(i);
+        for (int i = 0; i < t.length(); i++) result ^= t.charAt(i);
+        return (char) result;
     }
 }

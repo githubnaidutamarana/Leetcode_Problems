64ca8010c99c754da5490465b44ccfe5e8a98937
NAIDU TAMARANA <149986263+githubnaidutamarana@users.noreply.github.com>
2025-02-21 10:44:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (87.95%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -2,11 +2,9 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
          
         int[] result = new int[nums.length];
-       
         int start=0;
         int index=0;
         int mid=n;
-
         while(mid<nums.length){
             result[index++] = nums[start++];
             result[index++] = nums[mid++];
